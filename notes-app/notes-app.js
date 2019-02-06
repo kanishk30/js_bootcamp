aa620bb2b771085164658b364852cb5f40f8052e
kanishk30 <kanishk71@gmail.com>
2019-02-06 09:16:57 +0530

    Todo app - initial commit

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,13 @@
+const notes2 = [{
+        title: 'My next trip',
+        body: 'Visit Australia',
+    },
+    {
+        title: 'My nickname',
+        body: 'Kanu',
+    }
+];
+
 const el = document.querySelector('p')
 console.dir(el);
 el.innerText = 'Text changed';
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,8 +1,36 @@
-// Remove all p tags that have 'do'  in the text
-
-const ps = document.querySelectorAll('p');
-ps.forEach((p) => {
-    if(p.innerHTML.toLowerCase().includes('DO'.toLowerCase())) {
-        p.remove();
+const todos = [{
+        text: 'Yoga',
+        status: true
+    },
+    {
+        text: 'Learn',
+        status: false
+    },
+    {
+        text: 'Work',
+        status: true
+    },
+    {
+        text: 'Exercise',
+        status: false
+    },
+    {
+        text: 'Relax',
+        status: false
     }
+]
+// print status : false number of todos left
+// Add a p for each todo above using textValue
+
+let _todo = todos.filter((todo) => {
+    return !todo.status;
+});
+const summary = document.createElement('h2');
+summary.textContent = `You have ${_todo.length} todo left`;
+document.querySelector('body').appendChild(summary);
+
+todos.forEach((todo)=> {
+    const newTodo = document.createElement('p');
+    newTodo.textContent = todo.text;
+    document.querySelector('body').appendChild(newTodo);
 })
